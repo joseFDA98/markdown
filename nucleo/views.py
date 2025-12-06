bcef056ec20ecb6476978db47b6eb9c142172539
joseFDA98 <joseflodan@gmail.com>
2025-12-06 12:30:19 -0600

    se arreglo la vista del archivo en static

--- nucleo/views.py
+++ nucleo/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def index(request):
 
     context = {
-        "link_infografia": "/static/infografia_markdown_kit.png",
+        "link_infografia": "https://i.postimg.cc/vZm8hGLy/infografia-markdown-kit.png",
         "link_guia_pdf": "https://drive.google.com/file/d/18nkQbTFr9D7H2SbS7l5CclC30x3NQYyR/preview",
         "link_presentacion_slides": "https://docs.google.com/presentation/d/1U-y4GK-R2F9dKMSupUNpXbajrhl0YfsY/preview",
         "link_video_tutorial": "https://youtu.be/OpPGgiUCZx4?si=OwhHkCOt-5GckTfU",
